9994e13644d4ad96010c103a951c32108cbd5fba
op <mdopi35@gmail.com>
2023-06-23 00:30:16 +0600

    hi showrov

--- store/views.py
+++ store/views.py
@@ -16,3 +16,7 @@ def checkout(request):
     context = {}
     return render(request,'store/checkout.html',context)
 
+def index(request):
+    diction = {}
+    return render(request, 'store/index.html', context=diction)
+
