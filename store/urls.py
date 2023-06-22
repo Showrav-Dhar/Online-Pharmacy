2e6a60c024176d60163f971a280ddc4366b17af6
showrav dhar <dhardip349900@gmail.com>
2023-06-22 13:58:06 +0600

    second upd

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from store import views
+
+
+urlspatterns = [
+    # leave as empty string for base url
+    path('',views.store, name="store"), # this is the home page
+    path('cart/',views.cart, name="cart"),
+    path('checkout',views.checkout, name="checkout"),
+
+    #try changing the name if everything works fine after running server
+]
