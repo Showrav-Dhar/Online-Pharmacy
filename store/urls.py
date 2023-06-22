e6464e736fab28dadaa389bccc2e59ff44056189
showrav dhar <dhardip349900@gmail.com>
2023-06-22 14:09:19 +0600

    fin

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from store import views
 
 
-urlspatterns = [
+urlpatterns = [
     # leave as empty string for base url
     path('',views.store, name="store"), # this is the home page
     path('cart/',views.cart, name="cart"),
-    path('checkout',views.checkout, name="checkout"),
-
+    path('checkout/',views.checkout, name="checkout")
     #try changing the name if everything works fine after running server
 ]
