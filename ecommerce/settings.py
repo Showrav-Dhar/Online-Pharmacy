d293a14189f19cd9b66c74707fd90a4bf49fc018
showrav dhar <dhardip349900@gmail.com>
2023-06-23 12:44:06 +0600

    upd

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig'
+    ''
 ]
 
 MIDDLEWARE = [
@@ -116,12 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    # os.path.join(BASE_DIR, 'static')
-    #inside the base directory we are looking for the folder called static
 ]
 
 # Default primary key field type
