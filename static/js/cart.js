25eb24617dc78b6487dd82066a033cb0575a9590
showrav dhar <dhardip349900@gmail.com>
2023-07-22 20:35:39 +0600

    working on cart js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i<updateBtns.length; i++){
+    updateBtns[i].addEventListener('click',function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('Click event triggered for productId:',productId, 'action:', action)
+
+        console.log('USER : ',user)
+        if (user == 'AnonymousUser'){
+            console.log('Not Logged in')
+        }else{
+            console.log('User is logged in , Sending Data....')
+        }
+    })
+}
