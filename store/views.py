8c4b525f7585e5e97d7bf9eccf7af5d55d28cbdd
showrav dhar <dhardip349900@gmail.com>
2023-08-17 02:27:52 +0600

    upd

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ import json
 from django.http import HttpResponse
 from urllib.parse import unquote
 import datetime
-from .utils import cookieCart,cartData
+from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
 
@@ -81,28 +81,59 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = data['form']['total']
-        order.transaction_id = transaction_id
-
-
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer = customer,
-                order = order,
-                address = data['shipping']['address'],
-                city = data['shipping']['city'],
-                state = data['shipping']['state'],
-                zipcode = data['shipping']['zipcode'],
-            )
-
+    
     else:
         print('User Not Logged In..')
+        
+        print('COOKIES: ', request.COOKIES)
+        name = data['form']['name']
+        email = data['form']['email']
+
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+
+        customer,created = Customer.objects.get_or_create()(
+            email = email,
+        )
+        customer.name = name
+        customer.save()
+
+
+        order.objects.create(
+            customer = customer,
+            complete = False
+        )
+
+        for item in items:
+            product = Product.objects.get(id = item['product']['id'])
+
+            orderItem = OrderItem.objects.create(
+                product = product,
+                order = order,
+                quantity = item['quantity']
+            )
+
+    total = float(data['form']['total'])
+    # total = data['form']['total'] try this if float creates problem
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode'],
+        )
+
+
     # below line JsonResponse was in the tutorial
-    return JsonResponse('Payment Complete', safe=False) 
+    return JsonResponse('Your Payment Is Complete', safe=False) 
     
     #above line working
     # res = json.dumps({'success': 1,'msg': 'Payment Complete'})
