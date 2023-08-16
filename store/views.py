a7848468fdd2336db79e2eb8b42d15845d3a6deb
showrav dhar <dhardip349900@gmail.com>
2023-08-16 21:08:37 +0600

    upd

--- store/views.py
+++ store/views.py
@@ -34,9 +34,27 @@ def cart(request):  # this has some problems , may not work correctly
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print('cart')
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping':False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+
+            product = Product.objects.get(id = i)
+            total = (product.price*cart[i]["quantity"])
+
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]["quantity"]
+
+            items = {}
+
 
     context = {'items': items, 'order': order,'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
